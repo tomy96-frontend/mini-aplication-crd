deeb296fa17a3b628041d02c8071da55ba937dd4
tomy96 <serestomy@gmail.com>
2021-12-07 19:49:34 +0100

    first commit

---
+++ js/App.js
@@ -0,0 +1,24 @@
+let state = {
+   iceCream: [
+
+        {
+            name: 'Chocolate Ice Cream',
+            price: 30,
+            isInStock: true
+        },
+        {
+            name: 'Vanilla Ice Cream',
+            price: 25,
+            isInStock: false
+        },
+        {
+            name: 'Strawberry Ice Cream',
+            price: 60,
+            isInStock: true
+        },
+
+    ]
+}
+
+
+
